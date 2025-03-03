4d427fad273461cc549f9a69006b99a1265f6348
jonatas <jonatasabnersb@gmail.com>
2025-03-03 09:35:31 -0300

    adding information in the modal

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+let modalQt = 1
 const dq = (element) => document.querySelector(element)
-const dqAll = (elements) => document.querySelector(elements)
+const dqAll = (elements) => document.querySelectorAll(elements)
 
 pizzaJson.map((item, index) => {
     let pizzaItem = dq('.pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$: ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
 
 
+
     pizzaItem.querySelector('a').addEventListener('click', (event) => {
         event.preventDefault()
+
+        let key = event.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+        dq('.pizzaBig img').src = pizzaJson[key].img
+        dq('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        dq('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        dq('.pizzaInfo--actualPrice').innerHTML = `R$: ${pizzaJson[key].price.toFixed(2)}`
+        dq('.pizzaInfo--size.selected').classList.remove('selected')
+        dqAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+
+            size.querySelector('span').innerHTML = `${pizzaJson[key].sizes[sizeIndex]}`
+        });
+        dq('.pizzaInfo--qt').innerHTML = modalQt
 
         dq('.pizzaWindowArea').style.opacity = 0
         dq('.pizzaWindowArea').style.display = 'flex'
@@ -24,6 +44,7 @@ pizzaJson.map((item, index) => {
 
 
     })
+
 
     dq('.pizza-area').append(pizzaItem)
 });
