9a5c59e95e6e6c45925469ba06eb0029b9750e97
jonatas <jonatasabnersb@gmail.com>
2025-03-03 08:43:24 -0300

    adding pie window

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$: ${item.price.toFixed(2)}`
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
+
+    pizzaItem.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault()
+
+        dq('.pizzaWindowArea').style.opacity = 0
+        dq('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(
+            () => {
+                dq('.pizzaWindowArea').style.opacity = 1
+            }, 300
+        )
+
+
+
+    })
 
     dq('.pizza-area').append(pizzaItem)
 });
